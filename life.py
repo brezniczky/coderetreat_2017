88ef123822b34233cfa60e0e1a6979f27dfaae3c
Janos Brezniczky <jb@coriolistechnologies.com>
2017-11-18 15:26:10 +0000

    it's alive!!!

--- life.py
+++ life.py
@@ -1,3 +1,6 @@
+from time import sleep
+from random import random, seed
+
 class GameOfLife():
 
     def __init__(self):
@@ -7,12 +10,92 @@ class GameOfLife():
         h = len(self._board)
         return h, 0 if h == 0 else len(self._board[0])
 
+    def _create_board(self, size):
+        return [[False] * size[1] for y in range(size[0])]
+
     def resize(self, size):
-        self._board = [[False] * size[1] for y in range(size[0])]
+        self._board = self._create_board(size)
 
     def get_board(self):
         return self._board
 
     def set_is_alive(self, x, y, is_alive):
-        print self._board
         self._board[x][y] = is_alive
+
+    def get_board_as_display_at(self, x, y):
+        if not self._board[x][y]:
+            return " "
+        else:
+            return "*"
+
+    def board_to_display_str(self):
+        display = ""
+        for number in range(self.get_size()[0]):
+            col = self._board[number]
+            display = display + "".join([self.get_board_as_display_at(number, y) for y in range(len(col))]) + "\n"
+        return display
+
+    def will_it_live(self, x, y):
+        counter = 0
+
+        # x_range = range(max(x - 1, 0), min(x + 1, len(self._board) - 1) + 1)
+
+        # y_range = range(max(y - 1, 0), min(y + 1, (self.get_size()[1] - 1) + 1))
+        # # y_range = range(y - 1, y + 2)
+
+        for i in range(x - 1, x + 2):
+            for k in range(y - 1, y + 2):
+                if i < 0 or i >= self.get_size()[0]:
+                    continue
+                if k < 0 or k >= self.get_size()[0]:
+                    continue
+
+                if i == x and k == y:
+                    pass
+                elif self._board[i][k]:
+                    counter = counter + 1
+        if counter < 2:
+            return False  # the cell is dead
+        if counter > 3:
+            return False  # the cell is dead
+        if counter == 3:
+            """ the cell is either alive, and stays like
+                that, or comes back to life """
+            return True
+        return self._board[x][y]
+
+    def iterate(self):
+        # TODO: add test
+        size = self.get_size()
+        new_board = self._create_board(size)
+        for x in range(0, size[0]):
+            for y in range(0, size[1]):
+                new_board[x][y] = self.will_it_live(x, y)
+        self._board = new_board
+
+    def anim_print(self, iterations):
+        for i in range(iterations):
+            print self.board_to_display_str()
+            self.iterate()
+            sleep(1)
+
+    def randomize_board(self):
+        size = self.get_size()
+        for i in range(size[0]):
+            for j in range(size[1]):
+                self.set_is_alive(i, j, random() > 0.5)
+
+if __name__ == "__main__":
+    life = GameOfLife()
+    # print dir(life)
+    life.resize((40, 40))
+    life.randomize_board()
+    life.set_is_alive(3, 3, True)
+    life.set_is_alive(3, 2, True)
+    life.set_is_alive(3, 4, True)
+    # print life.board_to_display_str()
+    # life.iterate()
+    # print life.board_to_display_str()
+
+    # # print life.board_to_display_str()
+    life.anim_print(240)
--- test.py
+++ test.py
@@ -25,6 +25,21 @@ class TestGameOfLife(unittest.TestCase):
                                          [False, False, False]]
         )
 
+    def test_set_cell_32_23(self):
+        # captures the bug incurred with developing the
+        # implementation for rule testing test case change request
+        self._tested.resize((5, 5))
+        self._tested.set_is_alive(2, 3, True)
+        self._tested.set_is_alive(3, 2, True)
+        self.assertTrue(
+            self._tested.get_board() ==
+                [[False, False, False, False, False],
+                 [False, False, False, False, False],
+                 [False, False, False, True, False],
+                 [False, False, True, False, False],
+                 [False, False, False, False, False]]
+        )
+
     def test_set_cell_sets_True(self):
         self._tested.resize((2, 2))  # that should be a setup thing
         self._tested.set_is_alive(1, 1, True)
@@ -34,5 +49,95 @@ class TestGameOfLife(unittest.TestCase):
         )
 
 
+    def test_board_as_display_at(self):
+        self._tested.resize((1, 1))
+        self.assertEqual(
+            self._tested.get_board_as_display_at(0, 0),
+            " "
+        )
+
+    def test_board_as_display_at(self):
+        self._tested.resize([2, 2])
+        self._tested.set_is_alive(1, 1, True)
+        self.assertEqual(
+            self._tested.get_board_as_display_at(1, 1),
+            "*"
+        )
+
+    def test_board_as_display_at(self):
+        self._tested.resize([2, 2])
+        self._tested.set_is_alive(0, 0, True)
+        self.assertEqual(
+            self._tested.get_board_as_display_at(0, 0),
+            "*"
+        )
+
+    def test_print_board_all_empty(self):
+        self._tested.resize((1, 1))
+        self.assertEqual(
+            self._tested.board_to_display_str(),
+            " \n"
+        )
+
+    def test_print_board_one_live(self):
+        self._tested.resize((1, 1))
+        self._tested.set_is_alive(0, 0, True)
+        self.assertEqual(
+            self._tested.board_to_display_str(),
+            "*\n"
+        )
+
+    def test_print_board_twobytwo(self):
+        self._tested.resize((2, 2))
+        self.assertEqual(
+            self._tested.board_to_display_str(),
+            "  \n  \n"
+        )
+
+    def test_print_board_twobytwo(self):
+        self._tested.resize((3, 2))
+        self._tested.set_is_alive(2, 1, True)
+        self.assertEqual(
+            self._tested.board_to_display_str(),
+            "  \n  \n *\n"
+        )
+
+    def test_will_it_live(self):
+        self._tested.resize((5, 5))
+        self._tested.set_is_alive(3, 3, False)
+        self.assertEquals(
+            # cell with no neighbors stays dead
+            self._tested.will_it_live(3, 3),
+            False
+        )
+
+
+    def test_will_it_live_corner(self):
+        self._tested.resize((3, 3))
+        self._tested.set_is_alive(0, 0, True)
+        self.assertEquals(
+            # cell with no neighbors stays dead
+            # (assume cells outside the board are dead)
+            self._tested.will_it_live(0, 0),
+            False
+        )
+
+
+    def test_will_it_live_with_5_neighbors_live_cell(self):
+        self._tested.resize((5, 5))
+        self._tested.set_is_alive(3, 3, True)
+        self._tested.set_is_alive(2, 3, True)
+        self._tested.set_is_alive(3, 2, True)
+        self._tested.set_is_alive(4, 3, True)
+        self._tested.set_is_alive(3, 4, True)
+        self._tested.set_is_alive(4, 4, True)
+        self.assertEquals(
+            # 3. live cell with 3+ neighbours dies
+            self._tested.will_it_live(3, 3),
+            False
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
+
